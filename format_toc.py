50a4a0f0e7d0c658048260d3998b4cd1c2e195d5
Ian Davis <none@none>
2015-12-13 21:57:25 -0600

    Adding manual construction of table of contents at top of file

--- format_toc.py
+++ format_toc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import re
 from collections import defaultdict
 
 # Read the table of contents template
@@ -32,11 +33,34 @@ if used_sections - toc_sections:
     for section in used_sections - toc_sections:
         print("    %s" % section)
 
+header_rx = re.compile(r'^(#+) *(.+?) *(\1) *\n?$')
+non_alpha_rx = re.compile(r'[^a-z0-9.]+')
+def parse_header(line):
+    m = header_rx.match(line)
+    if not m: return None
+    depth = len(m.group(1))
+    title = m.group(2)
+    anchor = non_alpha_rx.sub('-', title.lower())
+    return depth, title, anchor
+
 with open("toc.md", "wt") as outfile:
     for line in toc:
+        header = parse_header(line)
+        if header is not None:
+            depth, title, anchor = header
+            outfile.write("%s* [%s](#%s)\n" % ("    "*(depth-1), title, anchor))
+    outfile.write("\n\n\n")
+    # Print out all lines in the template, replacing `;sections` with lists of recipes.
+    for line in toc:
+        header = parse_header(line)
+        if header is not None:
+            depth, title, anchor = header
+            outfile.write("<a name='%s'></a>\n" % anchor)
         if not line.startswith(";"):
             outfile.write(line)
             continue
         section = line[1:].strip()
         for recipe in sorted(recipes[section]):
             outfile.write("* %s\n" % recipe)
+
+# pandoc -f markdown_github -t html -o toc.html toc.md
